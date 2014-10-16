c0e54b88a05942b6aca196deaec4a738c27bf6c4
Charles Pletcher <cpletcher2@gmail.com>
2014-10-16 14:06:10 -0700

    Don't break on /favicon.ico

--- src/monsoon/controllers/home.clj
+++ src/monsoon/controllers/home.clj
@@ -1,10 +1,11 @@
 (ns monsoon.controllers.home
   (:use compojure.core)
-  (:require [ring.util.response :refer [header response status]]
+  (:require [ring.util.response :refer [header not-found response status]]
             [cheshire.core :refer [generate-string]]))
 
 (defroutes home-routes
   (ANY "/" [] (-> (response (generate-string {:status 200
                                               :message "Monsoon"}))
                   (status 200)
-                  (header "Content-Type" "application/json"))))
+                  (header "Content-Type" "application/json")))
+  (ANY "/favicon.ico" [] (not-found "Not Found")))
